700721f19515fbdbdd801a322964340f732f5d37
Henrique Medeiros <hmdros@gmail.com>
2019-02-14 18:34:24 -0200

    stil working on it

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,12 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-console.log('Hello World from Webpacker')
 
 import 'bootstrap';
+import { loadDynamicBannerText } from '../components/typed';
+loadDynamicBannerText();
+// javascript code goes here
+
+
+
+console.log('Hello World from Webpacker')
