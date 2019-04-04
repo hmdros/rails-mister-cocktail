2b962d0b0538c845f5977865bdd7e801f089edc2
Henrique Medeiros <hmdros@gmail.com>
2019-04-04 09:12:56 -0300

    atualizando"

---
+++ db/migrate/20190302230705_add_instruction_to_cocktail.rb
@@ -0,0 +1,5 @@
+class AddInstructionToCocktail < ActiveRecord::Migration[5.2]
+  def change
+    add_column :cocktails, :instruction, :text
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_15_172720) do
+ActiveRecord::Schema.define(version: 2019_03_02_230705) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 2019_02_15_172720) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "photo"
+    t.text "description"
+    t.text "instruction"
   end
 
   create_table "doses", force: :cascade do |t|
